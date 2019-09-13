98ad7e382f929999582caa37ef799f7429ed93cb
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-13 17:37:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 def badge_maker(name)
   
-  
+  "Hello, my name is #{name}."
   
 end
 
@@ -10,10 +10,10 @@ def batch_badge_creator(array)
   badge_array = []
   
   array.each do |badge|
-    
-    
-    
+    badge_array << badge_maker(badge)
   end
+  
+  badge_array
   
 end
 
