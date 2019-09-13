c2087996f910234567f9aa9a1aafc20a08339ad7
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-13 17:38:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,4 @@
+# Method that returns badge message w/ name
 def badge_maker(name)
   
   "Hello, my name is #{name}."
@@ -5,6 +6,7 @@ def badge_maker(name)
 end
 
 
+# Method that returns array of badge messages
 def batch_badge_creator(array)
   
   badge_array = []
